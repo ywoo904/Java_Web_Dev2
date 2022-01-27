54e1ce7b3fe5043bccf0ca43b9deaa3647b2cad5
ywoo904 <ywoo904@gmail.com>
2022-01-27 11:09:18 +0900

    Confirm_connection
    Confirm_connection

--- JDBC/src/com/jdbc/JDBCtest.java
+++ JDBC/src/com/jdbc/JDBCtest.java
@@ -1,10 +1,48 @@
 package com.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCtest {
 
-	public static void main (String[] args) {  }  
+	public static void main (String[] args) {    
 	
+	//오라클 설정 //DB접속 계정정보 
+	String driver_oracle = "oracle.jdbc.driver.OracleDriver";      //오라클 드라이버로딩 
+	String url_oracle= "jdbc:oracle:thin:@localhost:1521/XEPDB1"; //또는 IP 주소도 입력가능( cmd에서 확인)  
+	String user = "myjsp"; 
+	String password  ="myjsp"; 
+
+	//연결확인 위한 객체생성 	//connection 값을 판별하는 boolean값
+	Connection conn = null;
+	Boolean connect = false; 
 	
+	try { 
+		//1. 드라이버 로딩  
+		Class.forName(driver_oracle);
+
+		//2. DB연결 
+		conn = DriverManager.getConnection(url_oracle, user, password); 
+		connect = true; 
+	} catch (ClassNotFoundException cnfe) { 
+		 //1.드라이버로딩이 안될때 
+		connect= false; 
+		System.out.println("DB드라이버 로딩실패:"+ cnfe.toString() );
+	}  catch(SQLException sqle) {
+		//2. Connection 실패시 에러(SQL Exception)  
+		connect = false; 
+		System.out.println("DB접속 실패"+sqle.toString());
+	} catch (Exception e) { 
+		//Unknown 에러 
+		connect= false;
+		System.out.println("Unknown Error");
+		e.printStackTrace();
+	}  
 	
-	
-}
+	if (connect) { 
+		System.out.println("연결성공");
+	} else {  
+		System.out.println("연결실패");
+	}
+}} 
