7e66d1c248a547a56e8c24918c0b93323a334397
ywoo904 <ywoo904@gmail.com>
2022-01-27 11:37:21 +0900

    JDBCconnect
    JDBCconnect

--- JDBC/src/com/jdbc/JDBCtest.java
+++ JDBC/src/com/jdbc/JDBCtest.java
@@ -8,22 +8,28 @@ public class JDBCtest {
 
 	public static void main (String[] args) {    
 	
-	//오라클 설정 //DB접속 계정정보 
+	//오라클 설정,사용자DB접속 계정정보 
 	String driver_oracle = "oracle.jdbc.driver.OracleDriver";      //오라클 드라이버로딩 
 	String url_oracle= "jdbc:oracle:thin:@localhost:1521/XEPDB1"; //또는 IP 주소도 입력가능( cmd에서 확인)  
 	String user = "myjsp"; 
 	String password  ="myjsp"; 
 
+	//마리아DB설정 //MariaDB 사용자 설정: 
+	String driver_mariadb = "org.mariadb.jdbc.Driver";
+	String url_mariadb ="jdbc: mysql://localhost:3306/testdb"; 
+	String m_user="root";
+	String  m_password="0000"; 
+	
+	
 	//연결확인 위한 객체생성 	//connection 값을 판별하는 boolean값
 	Connection conn = null;
 	Boolean connect = false; 
 	
 	try { 
 		//1. 드라이버 로딩  
-		Class.forName(driver_oracle);
-
+		Class.forName(driver_oracle); //Class.forName(driver_mariadb);
 		//2. DB연결 
-		conn = DriverManager.getConnection(url_oracle, user, password); 
+		conn = DriverManager.getConnection(url_oracle, user, password); //conn= DriverManager.getConnection(url_mariadb, m_user, m_password);
 		connect = true; 
 	} catch (ClassNotFoundException cnfe) { 
 		 //1.드라이버로딩이 안될때 
