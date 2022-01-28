9fad71640d6f5e302f978447c9ce74c0a1c7c48c
ywoo904 <ywoo904@gmail.com>
2022-01-28 18:00:56 +0900

    update
    update

---
+++ JDBC/WebContent/update_success.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<h2>정보수정에 성공했습니다.  .  </h2>
+<a href= "mypage.jsp"> 마이페이지</a>
+
+</body>
+</html>
--- JDBC/src/com/ok/update_ok.java
+++ JDBC/src/com/ok/update_ok.java
@@ -1,6 +1,10 @@
 package com.ok;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +18,7 @@ public class update_ok extends HttpServlet {
 
     public update_ok() {
         super();
-        // TODO Auto-generated constructor stub
+      
     }
 
 
@@ -33,7 +37,70 @@ public class update_ok extends HttpServlet {
 		String id= request.getParameter("id"); 
 		String pw= request.getParameter("pw");
 		String name= request.getParameter("name");
-		String phone1= request .getParameter("phone1");
+		String phone1= request .getParameter("phone1"); 
+		String phone2= request .getParameter("phone2"); 
+		String email= request.getParameter("email"); 
+		String gender= request.getParameter("gender");
+		
+		System.out.println(id); 
+		System.out.println(pw); 
+		System.out.println(name); 
+		System.out.println(phone1); 
+		System.out.println(phone2); 
+		System.out.println(email); 
+		System.out.println(gender); 
+		
+		//DB연동 
+		String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1"; 
+		String user= "myjsp"; 
+		String password = "myjsp";
+		String driver = "oracle.jdbc.driver.OracleDriver"; 
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null; 
+		
+		String sql= "update testusers set pw=? , name=?, phone1=?, phone2=?."
+				+"email=?, gender=? where id=? "; 
+		try {
+			//1.드라이버로드 
+			Class.forName(driver);
+			//2. 커넥션 생성
+			conn= DriverManager.getConnection(url,user,password);
+			//3.PreparedStatement 객체생성
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1,pw); 
+			pstmt.setString(2,name); 
+			pstmt.setString(3,phone1); 
+			pstmt.setString(4,phone2); 
+			pstmt.setString(5,email); 
+			pstmt.setString(6,gender); 
+			pstmt.setString(7,id); 
+			
+			int result = pstmt.executeUpdate();
+			
+			if (result==1) { //성공시 성공페이지 이동 
+				response.sendRedirect("update_success.jsp");
+				
+			} else {  //실패시 마이페이지로 이동
+				response.sendRedirect("update_fail.jsp");
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace(); 
+		} finally {
+			try {
+				if (conn!=null) conn.close(); 
+				if (pstmt !=null) pstmt.close(); 
+				
+			} catch(Exception e) { } 
+			
+		} 
+		
+		
+		
+		
+		
 		
 		
 	}
